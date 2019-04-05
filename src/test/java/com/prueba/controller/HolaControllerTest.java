27082da9450b8bbdd500bde62211c8011ad6d36b
wilgustavo <wilgustavo@gmail.com>
2019-04-05 19:20:32 -0300

    Probando con WebMvcTest

--- src/test/java/com/prueba/controller/HolaControllerTest.java
+++ src/test/java/com/prueba/controller/HolaControllerTest.java
@@ -6,14 +6,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(HolaController.class)
 public class HolaControllerTest {
 
   @Autowired
